d55d4e86c7e05b99c752d41e9cb59497cb23e87c
Srconde <srconde@esei.uvigo.es>
2016-10-01 00:16:05 +0200

    Comentarios en HTTPRequest.java y añadido guardar el content

--- src/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
+++ src/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
@@ -17,13 +17,17 @@ public class HTTPRequest {
 	private ArrayList<String> pathList = new ArrayList<String>();
 	private Map<String, String> headerParameters = new HashMap<String, String>();
 	private Map<String, String> resourceParameters = new HashMap<String, String>();
+	private StringBuilder content = new StringBuilder();
 
 	public HTTPRequest(Reader reader) throws IOException, HTTPParseException {
+	
 		
 		 BufferedReader bufferedReader = new BufferedReader(reader);
 	        
 		 StringBuilder request = new StringBuilder();
 		 String line;	
+		 
+		 // Se lee la primera linea
 		 
 		 line = bufferedReader.readLine();
 		 
@@ -52,39 +56,63 @@ public class HTTPRequest {
 //			 }
 //		 }		
 		 
+		 // Se crea una regla que va a reconocer el método, el path y la version
+		 
 		 Pattern pattern = Pattern.compile("([A-Z]+) (.*) (HTTP/[0-9.]+)");
 		 Matcher matcher = pattern.matcher(line);
 		 matcher.find();
+		 
+		 //Se asignan los valores encontrados a sus variables
 		 
 		 this.method = HTTPRequestMethod.valueOf(matcher.group(1));
 		 this.resourceChain = matcher.group(2);
 		 this.httpVersion = matcher.group(3);
 		 
+		 //Se crea otro patron para coger nombre y parametros del recurso a partir de resourceChain
+		 
 		 pattern = Pattern.compile("/([^?]*)\\??(.*)");
 		 matcher = pattern.matcher(resourceChain);
 		 matcher.find();
 		 
+		//Se asignan los valores encontrados a sus variables
+		 
 		 this.resourceName = matcher.group(1);
 		 String parameters = matcher.group(2);
+		 
+		 //Se crea otro patron para coger el path a partir de resourceName
 		 
 		 pattern = Pattern.compile("([^/].[^/]*)");
 		 matcher = pattern.matcher(resourceName);
 		 
+		 //Se crea una lista con los valores que va a tener el array del Path
+		 
 		 while (matcher.find()) {
 			 pathList.add(matcher.group(1));
 		 }
+		 
+		 //Se crea un array de String resourcePath con los valores guardados en la lista anterior
+		 
 		 resourcePath = new String[pathList.size()];
 		 pathList.toArray(resourcePath);
 		 
+		 //Se crea un patron para los parametros del recurso de forma param1=valor1
+		 
 		 pattern = Pattern.compile("([^&].[^&]*)=([^&].[^&]*)");
 		 matcher = pattern.matcher(parameters);
+		 
+		 //Se van añadiendo estos parametros al mapa <parametro,valor> resourceParameters
 		 
 		 while (matcher.find()) {
 			 resourceParameters.put(matcher.group(1), matcher.group(2));
 		 }
 		 
+		 //Se crea un patron para coger los parametros de la cabecera en las siguientes linea
+		 
 		 pattern = Pattern.compile("([A-Za-z-/]+): (.+)");
 		 System.out.println(line);
+		 
+		 //Se van añadiendo los parametros al mapa<parametro,valor> headerParameters
+		 
 		 while ((line = bufferedReader.readLine()) != null) {
 			 System.out.println(line);
 			 matcher = pattern.matcher(line);
@@ -92,7 +120,19 @@ public class HTTPRequest {
 				 headerParameters.put(matcher.group(1), matcher.group(2));
 			 }
 		 }
-		 System.out.println(this.toString());
+		 
+		 //Guarda el content en un string
+		 
+		 content = null;
+		 
+		 while ((line = bufferedReader.readLine()) != null) {
+			 
+			 content.append(line);			 			
+			 
+		 }
+		 
+		 
+		 //System.out.println(this.toString());
 	}
 
 	public HTTPRequestMethod getMethod() {
@@ -132,12 +172,21 @@ public class HTTPRequest {
 
 	public String getContent() {
 		// TODO Auto-generated method stub
-		return null;
+		if (content == null){
+			return null;
+		}else{
+			return content.toString();
+		}
 	}
 
 	public int getContentLength() {
 		// TODO Auto-generated method stub
-		return -1;
+		
+		if (content == null){
+			return 0;
+		}else{
+			return content.length();
+		}
 	}
 
 	@Override
