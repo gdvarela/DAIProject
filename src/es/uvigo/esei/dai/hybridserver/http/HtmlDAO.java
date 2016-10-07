12802dec70c0db7b957a1d296cab03ebde8569c6
gdvarela <guillermo.davilavarela@gmail.com>
2016-10-07 12:58:16 +0200

    HtmlDao

---
+++ src/es/uvigo/esei/dai/hybridserver/http/HtmlDAO.java
@@ -0,0 +1,10 @@
+package es.uvigo.esei.dai.hybridserver.http;
+
+//Gestionar Contenido Mapa Provisional Ficheros;
+//Get, Create, Delete, list
+//Luego crearlo como Interfaz para sacar El mapa de strings como una implementación concreta de la interfaz
+//Cuando añadamos la BD no será necesário cambiar la interfaz
+
+public class HtmlDAO {
+
+}
